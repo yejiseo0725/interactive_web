1015b79785204b4a054afbc288c097af808097da
keroppi <yejiseo0725@gmail.com>
2025-01-30 23:07:32 +0900

    js_3d scroll: resizeHandler

--- js/wall3d.js
+++ js/wall3d.js
@@ -2,13 +2,21 @@
   const houseElem = document.querySelector(".house");
 
   // 전체 문서 높이; 스크롤바 트랙의 높이; body의 높이
-  // 전체 높이 - 스크롤바 제외 높이 = 스크롤 가능 범위
-  let maxScrollValue = document.body.offsetHeight - window.innerHeight;
+  // 현재 문서 높이 - 윈도우 높이 = 스크롤 가능 범위
+  let maxScrollValue = 0;
+
+  function resizeHandler() {
+    maxScrollValue = document.body.offsetHeight - window.innerHeight;
+  }
 
   window.addEventListener("scroll", function () {
     // 비율: 0~1
     // 490 은 css transfrom 의 default 값을 빼준 것
-    const zMove = (scrollY / maxScrollValue) * 980 - 490;
+    const zMove = (scrollY / maxScrollValue) * 970 - 490;
     houseElem.style.transform = "translateZ(" + zMove + "vw)";
   });
+
+  window.addEventListener("resize", resizeHandler);
+  // 초기화를 위해 일부러 실행
+  resizeHandler();
 })();
