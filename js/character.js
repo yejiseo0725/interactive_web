5a6b08fbe9f343314a1f74ee0f42fa44a4a6890a
keroppi <65063305+yejiseo0725@users.noreply.github.com>
2025-02-04 01:03:44 +0900

    js_3d scroll: character legs & arms animation

--- js/character.js
+++ js/character.js
@@ -4,6 +4,8 @@ function Character(info) {
   // 의미: <div></div>
   this.mainElem.classList.add("character");
   // 의미: <div class="character"></div>
+  // this.mainElem.classList.add("character", "running");
+  // add 로 여러 개의 class 넣을 때는 ,(쉼표)로 구분해주기
   this.mainElem.innerHTML =
     "" +
     '<div class="character-face-con character-head">' +
@@ -35,3 +37,9 @@ function Character(info) {
 
   this.mainElem.style.left = info.xPos + "%";
 }
+
+// prototype 객체의 재설정 (기존것을 변화시키기 위함)
+Character.prototype = {
+  // constructor 속성이 가리키는 것: 생성자 (기존것을 일단 보존하기 위해 따로 지정해 줌)
+  constructor: Character,
+};
