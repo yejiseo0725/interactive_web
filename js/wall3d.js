14cc304f5bdc66e1be67062227759e2a0cca69f9
keroppi <yejiseo0725@gmail.com>
2025-02-07 00:17:50 +0900

    js_3d scroll: data-char 로 캐릭터 변경하기

--- js/wall3d.js
+++ js/wall3d.js
@@ -2,6 +2,7 @@
   const stageElem = document.querySelector(".stage");
   const houseElem = document.querySelector(".house");
   const barElem = document.querySelector(".progress-bar");
+  const selectCharacterElem = document.querySelector(".select-character");
   const mousePos = { x: 0, y: 0 };
 
   // 전체 문서 높이; 스크롤바 트랙의 높이; body의 높이
@@ -46,6 +47,11 @@
       xPos: (e.clientX / window.innerWidth) * 100,
       speed: Math.random() + 0.15,
     });
+  });
+
+  selectCharacterElem.addEventListener("click", function (e) {
+    const value = e.target.getAttribute("data-char");
+    document.body.setAttribute("data-char", value);
   });
 
   // 초기화를 위해 일부러 실행
